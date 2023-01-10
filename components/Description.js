d9217207f08cc9b7de0e65f100ecdef2bf9bf8f2
JeremyThiebaut <jeremy.thiebaut.pro@gmail.com>
2023-01-10 21:08:20 +0100

    fix bugs

--- components/Description.js
+++ components/Description.js
@@ -4,18 +4,17 @@ import Image from "next/image";
 const Description = ({ profil }) => {
   const picture = profil.picture[0].url;
   return (
-    <>
-      <div className={styles.description}>
-        <p>{profil.description}</p>
-      </div>
+    <div className={styles.description}>
+      <p className={styles.description__text}>{profil.description}</p>
       <Image
+        className={styles.description__picture}
         src={picture}
         alt="Image de profil"
         width={500}
         height={500}
         priority
       />
-    </>
+    </div>
   );
 };
 
