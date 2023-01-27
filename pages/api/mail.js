bb120907fb971cfaa62a143bf3a0f8b60cd5523d
JeremyThiebaut <jeremy.thiebaut.pro@gmail.com>
2023-01-27 11:46:54 +0100

    remove nodemailer and autosend mail

--- pages/api/mail.js
+++ pages/api/mail.js
@@ -1,7 +1,6 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 const { AIRTABLE_API_KEY, AIRTABLE_BASE_ID } = process.env;
 const Airtable = require("airtable");
-import { mailOptions, transporter } from "../../config/nodemailer";
 
 const handler = async (req, res) => {
   const base = new Airtable({ apiKey: AIRTABLE_API_KEY }).base(
@@ -39,22 +38,8 @@ const handler = async (req, res) => {
           return res.status(400).json({ message: e.message });
         });
 
-      const newMessage = data.message.split("\n").join("<br/>");
-      await transporter.sendMail({
-        from: `"${data.firstName} ${data.lastName}" <${data.mail}>'`,
-        ...mailOptions,
-        subject: "Email du site",
-        html: `<h1>Message envoyé depuis la page Contact de jtdev.fr</h1>
-        <p><b>Nom, Prénom : </b>${data.firstName} ${data.lastName}<br>
-        <b>Email : </b>${data.mail}<br>
-        <b>Numéro : </b>${data.phone}<br>
-        <b>Message : </b><br/>${newMessage}</p>`,
-      });
-
-      console.log("Send mail success.");
       return res.status(200).json({ success: true });
     } catch (e) {
-      console.log("Send mail failed.");
       return res.status(401).end();
     }
   } else {
